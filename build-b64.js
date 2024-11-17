51cf984d6bb56ae97b405b09d85420bb3d55fe28
lmchzromain <lemichez.r@gmail.com>
2024-11-17 16:12:45 +0100

    Add missing viewport meta tag in b64 generated file

--- build-b64.js
+++ build-b64.js
@@ -15,7 +15,8 @@ const html = `
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Hello world</title>
   <style>
     ${css}
