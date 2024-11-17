b05d8a823c03065bbd84b5f627725f79a42ad6df
lmchzromain <lemichez.r@gmail.com>
2024-11-17 15:15:01 +0100

    Add command to generate a static website encoded in base64

--- build-b64.js
+++ build-b64.js
@@ -2,8 +2,40 @@ require("@babel/register")({
   extensions: [".js", ".jsx"],
 });
 
+const fs = require("fs");
 const ReactDOMServer = require("react-dom/server");
 const { App } = require("./src/scripts/App");
 
-const html = ReactDOMServer.renderToStaticMarkup(App());
-console.log("html", html);
+const fileName = "index.b64.txt";
+
+const content = ReactDOMServer.renderToStaticMarkup(App());
+const css = fs.readFileSync("./dist/styles.css", "utf8");
+
+const html = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Hello world</title>
+  <style>
+    ${css}
+  </style>
+</head>
+<body>
+  ${content}
+</body>
+</html>
+`;
+
+fs.writeFile(
+  `./dist/${fileName}`,
+  `data:text/html;base64,${btoa(html)}`,
+  (err) => {
+    if (err) {
+      console.error("Error writing to the file:", err);
+    } else {
+      // eslint-disable-next-line no-console
+      console.log(`The file '${fileName}' has been successfully created !`);
+    }
+  }
+);
